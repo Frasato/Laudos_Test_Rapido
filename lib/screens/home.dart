f2a178da8e53070b2393fca4c87c9926a859b599
Frasato <gabriel.frasato@hotmail.com>
2025-09-15 16:58:19 -0300

    :sparkles: Home navigation menu

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,9 +10,40 @@ class Home extends StatefulWidget{
 class _HomeState extends State<Home>{
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(),
-    ),
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('lib/assets/bg.jpg'),
+          fit: BoxFit.cover
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Laudos Para Testes Rápidos', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: ()=>{Navigator.pushReplacementNamed(context, '/covid')},
+                  child: Text('Covid-19')
+                ),
+                SizedBox(width: 10,),
+                ElevatedButton(
+                  onPressed: ()=>{Navigator.pushReplacementNamed(context, '/dengue')},
+                  child: Text('Dengue')
+                ),
+              ],
+            ),
+          ],
+        )
+      ),
+    )
   );
 }
