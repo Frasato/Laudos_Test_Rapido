6c51b1d29a44cd2bd4d93648a8afcc5fa9e03cd2
Frasato <gabriel.frasato@hotmail.com>
2025-09-15 15:35:05 -0300

    :sparkles: feat: Create Home Page

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(),
+    ),
+  );
+}
